19a7f37bd114d5300dc7993661cc18ec8cd0b648
james <jamesmahato10@gmail.com>
2021-09-27 11:45:53 +0545

    search command implied.

--- weather_app.py
+++ weather_app.py
@@ -1,8 +1,56 @@
 from tkinter import *
-from tkinter import messagebox
+
 from configparser import ConfigParser
 import requests
-from PIL import Image,ImageTk
+url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+
+config_file = "config.init"
+config = ConfigParser()
+config.read(config_file)
+api_key = config['api_key']['key']
+
+def get_weather(city):
+    result = requests.get(url.format(city,api_key))
+    if result:
+       json = result.json()
+       #  (city,country,temp_fahrenheit,weather)
+       city = json["name"]
+       country = json["sys"]["country"]
+       temp_kelvin = json['main']['temp']
+       temp_celsius = temp_kelvin - 273.15
+       temp_fahrenheit = (temp_kelvin - 273.15)*9/5+32
+
+       weather = json["weather"][0]["main"]
+       final = [city,country,temp_celsius,temp_fahrenheit,weather]
+       return final
+
+    else:
+        return None
+
+
+
+
+
+def search():
+
+    city = city_text.get()
+    weather = get_weather(city)
+
+
+
+    # print(weather_state)
+
+    if weather:
+        location_lbl["text"] = '{},{}'.format(weather[0], weather[1])
+        temp_lbl["text"]= '{:.2f}°C, {:.2f}°F'.format(weather[2], weather[3])
+        weather_lbl["text"] = weather[4]
+
+
+
+
+    else:
+        pass
+
 
 
 
@@ -14,8 +62,6 @@ app.geometry("600x350")
 app.iconbitmap('icon.ico')
 app.config(bg='azure1')
 
-def search():
-    pass
 
 city_text = StringVar()
 city_entry = Entry(app, textvariable = city_text,bg='azure1')
